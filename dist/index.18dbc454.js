38efbe70e42fbac1267c4a83331655ec1c36172b
Suman313 <sumanmodak616@gmail.com>
2023-04-11 14:20:14 +0530

    mobile responsiveness added

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -557,25 +557,42 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
-const getAllSections = document.querySelectorAll("section");
+// const getAllSections = document.querySelectorAll('section');
 const getGlassMorphicSec = document.getElementById("glass-morphic");
+const getHambergerMenu = document.getElementById("hamberger-menu");
+const getHambergerMenuUl = document.getElementById("hamberger-icon");
+var menuToggle = false;
 window.onload = getGlassMorphicSec.classList.add("glass-show");
-// console.log(getGlassMorphicSec.classList);
-// console.log(getGlassMorphicSec)
-const options = {
-    root: null,
-    thresold: 0.7,
-    rootMargin: "-300px"
-};
-const observer = new IntersectionObserver((entries, observer)=>{
-    entries.forEach((entry)=>{
-        // console.log(entry.target)
-        console.log(entry);
-        if (entry.isIntersecting) entry.target.classList.add("show");
-        else entry.target.classList.remove("show");
-    });
-}, options);
-getAllSections.forEach((el)=>observer.observe(el));
+// // console.log(getGlassMorphicSec.classList);
+// // console.log(getGlassMorphicSec)
+// const options = {
+//     root: null,
+//     thresold: 0.7,
+//     rootMargin: "-300px",
+// }
+// const observer = new IntersectionObserver((entries, observer)=>{
+//     entries.forEach(entry=>{
+//         // console.log(entry.target)
+//         console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }
+//         else {
+//             entry.target.classList.remove("show")
+//         }
+//     })
+// }, options)
+// getAllSections.forEach(el=> observer.observe(el))
+getHambergerMenu.addEventListener("click", ()=>{
+    console.log("working");
+    if (menuToggle) {
+        getHambergerMenuUl.classList.remove("responsive-ul-show");
+        menuToggle = false;
+    } else {
+        menuToggle = true;
+        getHambergerMenuUl.classList.add("responsive-ul-show");
+    }
+});
 
 },{}]},["9tRox","1SICI"], "1SICI", "parcelRequire4d93")
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,44 @@
-const getAllSections = document.querySelectorAll('section');
+// const getAllSections = document.querySelectorAll('section');
 const getGlassMorphicSec = document.getElementById('glass-morphic')
+const getHambergerMenu = document.getElementById('hamberger-menu');
+const getHambergerMenuUl = document.getElementById('hamberger-icon')
+var menuToggle = false;
 window.onload = getGlassMorphicSec.classList.add("glass-show")
 
-// console.log(getGlassMorphicSec.classList);
-// console.log(getGlassMorphicSec)
-const options = {
-    root: null,
-    thresold: 0.7,
-    rootMargin: "-300px",
-}
+// // console.log(getGlassMorphicSec.classList);
+// // console.log(getGlassMorphicSec)
+// const options = {
+//     root: null,
+//     thresold: 0.7,
+//     rootMargin: "-300px",
+// }
 
-const observer = new IntersectionObserver((entries, observer)=>{
-    entries.forEach(entry=>{
-        // console.log(entry.target)
-        console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
+// const observer = new IntersectionObserver((entries, observer)=>{
+//     entries.forEach(entry=>{
+//         // console.log(entry.target)
+//         console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
             
 
-        }
-        else {
-            entry.target.classList.remove("show")
-        }
-    })
-}, options)
+//         }
+//         else {
+//             entry.target.classList.remove("show")
+//         }
+//     })
+// }, options)
 
-getAllSections.forEach(el=> observer.observe(el))
+// getAllSections.forEach(el=> observer.observe(el))
+
+getHambergerMenu.addEventListener("click", () =>{
+    console.log("working")
+    if(menuToggle) {
+        
+    getHambergerMenuUl.classList.remove("responsive-ul-show");
+    menuToggle = false;
+    }
+    else {
+    menuToggle = true;
+    getHambergerMenuUl.classList.add("responsive-ul-show");
+    }
+})
